b8efa74a4cc1451cf47dd24f9e05ba91952f4e19
umatbro <umatbroo@gmail.com>
2020-02-17 14:08:51 +0100

    init

---
+++ h2p/handler.py
@@ -0,0 +1,26 @@
+import json
+import pandas as pd
+
+def hello(event, context):
+    df = pd.DataFrame({'a': [1, 2, 3], 'b':[2,3,4]})
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        # "input": event
+        'df': df.to_dict(),
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
